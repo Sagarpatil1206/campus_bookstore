16dd984e5828ce85edea9f9f5eb0ba6101c4524f
Sagar Patil <sagarpatil120601@gmail.com>
2023-03-12 21:40:36 +0530

    changes in bgcolor,navbar

--- books_resale/src/components/Posts/Posts.js
+++ books_resale/src/components/Posts/Posts.js
@@ -2,21 +2,22 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
+import './Posts.css'
 
 const Posts = () => {
   const posts = useSelector((state)=>state.posts);
   console.log("In post section");
   console.log(posts);
   return (
-    <>
-      <Grid container spacing={3}>
+    <div className='postsPage'>
+      <Grid container spacing={3} marginTop='10px'>
         {posts.map((post)=>(
           <Grid item sm={4} key={post._id} >
             <Post post={post}/>
           </Grid>
         ))}
       </Grid>
-    </>
+    </div>
   );
 };
 
