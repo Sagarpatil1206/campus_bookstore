708d7ceb093c6f113551603b6baca5f6e73183f4
Sagar Patil <sagarpatil120601@gmail.com>
2023-03-27 12:33:44 +0530

    auth added

--- books_resale/src/components/Auth/Auth.js
+++ books_resale/src/components/Auth/Auth.js
@@ -1,79 +1,78 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './Auth.css'
 // import Logo from '../../images/logo.png'
 
 
-function  Auth () {
-  const [IsLogin, setIsLogin]=useState(true);
+function Auth() {
+  const [IsLogin, setIsLogin] = useState(true);
   // console.log(setdata);
-  const ToggleLogin = ()=>{
-    // e.preventDefault();
-    setIsLogin(!IsLogin);
-    // console.log();
+  const ToggleLogin = () => {
+    setIsLogin(!IsLogin)
   }
   return (
-    IsLogin ? (
-      <div className="a-right" style={{margin:'auto'}}>
-      <form className="infoForm authForm">
-        <h3>Sign Up</h3>
-        <div>
-          <input type="text" placeholder='First Name'
-          className='infoInput' name='firstname'/>
-           <input type="text" placeholder='Last Name'
-          className='infoInput' name='lastname'/>
+    <div className="Auth">
+      <div className="a-left">
+        {/* <img src={Logo} alt="" /> */}
+        <div className="Webname">
+          <h1>Book Resale</h1>
+          <h6>Use it, resale it</h6>
         </div>
-        <div>
-          <input type="text" placeholder='Username'
-          className='infoInput' name='username'/>
-        </div>
-        <div>
-          <input type="password" placeholder='Password'
-          className='infoInput' name='password'/>
-           <input type="password" placeholder='Confirm Password'
-          className='infoInput' name='confirmpassword'/>
-        </div>
-        <div>
-          <p > Already have an account. <button onClick={ToggleLogin}>Log In</button></p>
-          {/* <button onClick ={()=>setcount(count+1)}> LogIn</button>
+      </div>
+      {IsLogin ? (
+      <div className="a-right">
+        <form className="infoForm authForm">
+          <h3>Sign Up</h3>
+          <div>
+            <input type="text" placeholder='First Name'
+              className='infoInput' name='firstname' />
+            <input type="text" placeholder='Last Name'
+              className='infoInput' name='lastname' />
+          </div>
+          <div>
+            <input type="text" placeholder='Username'
+              className='infoInput' name='username' />
+          </div>
+          <div>
+            <input type="password" placeholder='Password'
+              className='infoInput' name='password' />
+            <input type="password" placeholder='Confirm Password'
+              className='infoInput' name='confirmpassword' />
+          </div>
+          <div>
+            <p > Already have an account. <button  className='button infoButton' onClick={ToggleLogin}>Log In</button></p>
+            {/* <button onClick ={()=>setcount(count+1)}> LogIn</button>
           { 
             count && <LogIn/>
           } */}
-        </div>
-        <button className='button infoButton' type='submit'>Signup</button>
-      </form>
-    </div>
-    ) : 
-    (
+          </div>
+          <button className='button infoButton' type='submit'>Signup</button>
+        </form>
+      </div>
+      ) :
+      (
       <div className="a-right">
-      <form className="infoForm authForm">
-        <h3>Log In</h3>
-        <div>
-          <input type="text" placeholder='Username'
-          className='infoInput' name='username'/>
-        </div>
-        <div>
-          <input type="text" placeholder='Password'
-          className='infoInput' name='password'/>
-        </div>
-        <div>
-          <span > Don't have an account!</span>
-          {/* <button className='button infoButton' type='submit'>Signup</button> */}
-          <button onClick={ToggleLogin}>Signup</button>
-        </div>
-        <button className='button infoButton' type='submit'>LogIn</button>
-      </form>
+        <form className="infoForm authForm">
+          <h3>Log In</h3>
+          <div>
+            <input type="text" placeholder='Username'
+              className='infoInput' name='username' />
+          </div>
+          <div>
+            <input type="text" placeholder='Password'
+              className='infoInput' name='password' />
+          </div>
+          <div>
+            <span > Don't have an account!</span>
+           
+            <button className='button infoButton' onClick={ToggleLogin}>Signup</button>
+          </div>
+          <button className='button infoButton' type='submit'>LogIn</button>
+        </form>
+      </div>
+      )}
+
+     
     </div>
-    )
-    // <div className="Auth">
-    //   <div className="a-left">
-    //     <img src={Logo}  alt=""/>
-    //     <div className="Webname">
-    //       <h1>Book Resale</h1>
-    //       <h6>Use it, resale it</h6>
-    //     </div>
-    //   </div>
-    //   <SignUp/>
-    // </div>
   );
 };
 
@@ -88,7 +87,7 @@ function  Auth () {
 
 // function LogIn(){
 //   return (
-    
+
 //   )
 // }
 
